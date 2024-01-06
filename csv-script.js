4a2742f5e4617c9d46d5398f0bd37985b37c97da
vladimir-monari <vladimir.monari@gmail.com>
2024-01-06 22:24:50 +0100

    Update js

--- csv-script.js
+++ csv-script.js
@@ -9,33 +9,47 @@ function loadCSV() {
     });
 }
 
-// Fonction pour générer les éléments d'image
 function generateImages(data) {
     var galleryContainer = document.querySelector('.image-container');
 
-    data.forEach(function(item) {
-        if (!item['Nom de l\'image'] || !item['Chemin de l\'image']) {
-            return; // Ignorez cette entrée
+    // Nombre d'images par colonne
+    var imagesPerColumn = 3;
+
+    // Ajouter une nouvelle colonne pour chaque groupe d'imagesPerColumn
+    for (var i = 0; i < data.length; i += imagesPerColumn) {
+        var column = document.createElement('div');
+        column.classList.add('column');
+
+        // Générer des images pour cette colonne
+        for (var j = i; j < i + imagesPerColumn && j < data.length; j++) {
+            var item = data[j];
+            if (!item['Nom de l\'image'] || !item['Chemin de l\'image']) {
+                continue; // Ignorer cette entrée
+            }
+
+            var img = createRandomSizeImage(item['Chemin de l\'image']);
+            img.alt = item['Description de l\'image'];
+            img.onclick = function () {
+                openModal(item['Chemin de l\'image']);
+            };
+
+            // Ajouter les tags à chaque image
+            var tags = item.Tags.split(',');
+            tags.forEach(function (tag) {
+                img.classList.add(tag.trim());
+            });
+
+            column.appendChild(img);
         }
 
-        var img = createRandomSizeImage(item['Chemin de l\'image']);
-        img.alt = item['Description de l\'image'];
-        img.onclick = function() {
-            openModal(item['Chemin de l\'image']);
-        };
-
-        // Ajouter les tags à chaque image
-        var tags = item.Tags.split(',');
-        tags.forEach(function(tag) {
-            img.classList.add(tag.trim());
-        });
-
-        galleryContainer.appendChild(img);
-    });
+        // Ajouter la colonne à la galerie
+        galleryContainer.appendChild(column);
+    }
 
     // Initialize Masonry after images are generated
     initMasonry();
 }
+
 
 function createRandomSizeImage(src) {
     const img = document.createElement('img');
