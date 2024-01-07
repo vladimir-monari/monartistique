fd3ddc31cb67c40a0025d0a6c6780b5b13bc6595
vladimir-monari <vladimir.monari@gmail.com>
2024-01-07 12:52:33 +0100

    fix js

--- scripts.js
+++ scripts.js
@@ -1,20 +1,44 @@
-d3.csv("images.csv").then(function(data) {
-    data.forEach(function(d) {
-        var img = new Image();
-        img.src = d.chemin;
-        img.alt = d.description;
-        img.classList.add("image-responsive");
+// Fonction pour charger un fichier CSV
+function loadCSV(callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'images.csv', true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            callback(xhr.responseText);
+        }
+    };
+    xhr.send();
+}
 
-        img.onclick = function() {
-            document.getElementById('modal').style.display = "block";
-            document.getElementById("modal-image").src = this.src;
-            document.getElementById("caption").innerHTML = this.alt;
-        };
+// Fonction pour générer les images à partir des données CSV
+function generateImages(data) {
+    var imageContainer = document.getElementById('image-container');
 
-        document.getElementById("image-container").appendChild(img);
+    // Utilisation de la bibliothèque PapaParse pour analyser le CSV
+    Papa.parse(data, {
+        header: true,
+        complete: function (results) {
+            results.data.forEach(function (d) {
+                var img = new Image();
+                img.src = d['Chemin de l\'image'];
+                img.alt = d['Description de l\'image'];
+                img.classList.add("image-responsive");
+
+                img.onclick = function () {
+                    document.getElementById('modal').style.display = "block";
+                    document.getElementById("modal-image").src = this.src;
+                    document.getElementById("caption").innerHTML = this.alt;
+                };
+
+                imageContainer.appendChild(img);
+            });
+        }
     });
-});
+}
 
-document.getElementsByClassName("close")[0].onclick = function() {
+// Chargement du CSV et génération des images
+loadCSV(generateImages);
+
+document.getElementsByClassName("close")[0].onclick = function () {
     document.getElementById('modal').style.display = "none";
 };
