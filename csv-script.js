cbe80a3b73dd14dab1154cc54131a7349429ca1a
vladimir-monari <vladimir.monari@gmail.com>
2024-01-07 11:45:36 +0100

    Add description

--- csv-script.js
+++ csv-script.js
@@ -15,26 +15,29 @@ function generateImages(data) {
 
     // Générer des images pour une seule ligne
     for (var i = 0; i < data.length; i++) {
-
         var item = data[i];
         if (!item['Nom de l\'image'] || !item['Chemin de l\'image']) {
             continue; // Ignorer cette entrée
         }
 
-        // Utiliser directement le chemin de l'image sans randomSizeImage
         var img = document.createElement('img');
         img.src = item['Chemin de l\'image'];
 
-        // Ajouter d'autres attributs ou styles si nécessaire
-        // img.alt = item['Nom de l\'image'];
-        // img.style.width = '100%';
+        var figCaption = document.createElement('figcaption');
+        figCaption.textContent = item['Description de l\'image'];
 
-        row.appendChild(img);
+        var figure = document.createElement('figure');
+        figure.classList.add('gallery-item');
+        figure.appendChild(img);
+        figure.appendChild(figCaption);
+
+        row.appendChild(figure);
     }
 
     // Ajouter la ligne à la galerie
     galleryContainer.appendChild(row);
 }
+
 
 // Appel de la fonction pour charger le CSV et générer les images
 loadCSV();
