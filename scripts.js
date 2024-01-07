d2e7cade011e8375daf6fe41b970fdb9945d0d0d
vladimir-monari <vladimir.monari@gmail.com>
2024-01-07 15:45:17 +0100

    Add tag filter

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
 // Fonction pour charger un fichier CSV
-// Fonction pour charger un fichier CSV
 function loadCSV(callback) {
     fetch('images.csv')
         .then(response => response.text())
@@ -15,18 +14,14 @@ function generateImages(data) {
         header: true,
         complete: function (results) {
             results.data.forEach(function (d) {
-                // Logique existante pour afficher les images...
                 var container = document.createElement('div');
                 container.classList.add('image-wrapper');
+                container.dataset.tags = d['Tags'];
 
                 var img = new Image();
                 img.src = d['Chemin de l\'image'];
                 img.alt = d['Description de l\'image'];
                 img.classList.add("image-responsive");
-
-                var description = document.createElement('div');
-                description.classList.add('image-description');
-                description.textContent = d['Description de l\'image'];
 
                 img.onclick = function () {
                     document.getElementById('modal').style.display = "block";
@@ -35,18 +30,8 @@ function generateImages(data) {
                 };
 
                 container.appendChild(img);
-                var descContainer = document.createElement('div'); // Nouveau conteneur pour la description
-                descContainer.classList.add('description-container'); // Nouvelle classe pour le style
-                descContainer.appendChild(description);
-                img.onload = function () {
-                    // Ajuster la largeur du conteneur de description à la largeur de l'image
-                    descContainer.style.maxWidth = `${img.width}px`;
-                };
-
-                container.appendChild(descContainer); // Ajoutez la description au nouveau conteneur
                 imageContainer.appendChild(container);
 
-                // Nouvelle logique pour les tags
                 d['Tags'].split('-').forEach(function (tag) {
                     if (tags[tag]) {
                         tags[tag]++;
@@ -64,16 +49,46 @@ function generateImages(data) {
 function displayTagCloud(tags) {
     var tagCloud = document.createElement('div');
     tagCloud.id = 'tag-cloud';
+    document.querySelector('#container h3').insertAdjacentElement('afterend', tagCloud);
+
+    var allTag = document.createElement('span');
+    allTag.textContent = 'tout ';
+    allTag.style.fontSize = '16px';
+    allTag.style.margin = '5px';
+    allTag.style.cursor = 'pointer';
+    allTag.addEventListener('click', function () {
+        filterImages('tout');
+    });
+    tagCloud.appendChild(allTag);
+
     for (var tag in tags) {
-        var size = 16 + (tags[tag] * 2); // Exemple de calcul de taille
+        var size = 16 + (tags[tag] * 2);
         var span = document.createElement('span');
         span.textContent = tag + ' ';
         span.style.fontSize = size + 'px';
         span.style.margin = '5px';
+        span.style.cursor = 'pointer';
+
+        span.addEventListener('click', function (event) {
+            filterImages(event.target.textContent.trim());
+        });
+
         tagCloud.appendChild(span);
     }
-    var galleryTitle = document.querySelector('#container h3');
-    galleryTitle.insertAdjacentElement('afterend', tagCloud);
+}
+
+function filterImages(selectedTag) {
+    var imageContainers = document.querySelectorAll('.image-wrapper');
+
+    imageContainers.forEach(function (container) {
+        var tags = container.dataset.tags.split('-');
+
+        if (selectedTag === 'tout' || tags.includes(selectedTag)) {
+            container.style.display = 'block';
+        } else {
+            container.style.display = 'none';
+        }
+    });
 }
 
 // Chargement du CSV et génération des images
